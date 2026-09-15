6294839f9f2c4855110ff89ff5a17fca5176ca7b
Mrbalwanek <jakub.cyranowski@gmail.com>
2026-09-15 10:40:52 +0200

    inital commit

---
+++ app/src/main/java/com/example/demoemptyactivity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.demoemptyactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button registerBtn = findViewById(R.id.btn1);
+        Button loginBtn = findViewById(R.id.btn2);
+
+        loginBtn.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
